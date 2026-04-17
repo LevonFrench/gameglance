09bb379734ee972d9bc04d8af52aebbf91ec102f
LevonFrench <foxhoundsounds@gmail.com>
2026-04-16 23:26:16 -0500

    chore: remove obsolete root debugging scripts

--- fix_jacko.py
+++
@@ -1,7 +0,0 @@
-
-with open('src/games.ts', 'r', encoding='utf-8') as f:
-    text = f.read()
-text = text.replace('Jack-O\ (Coming Soon)\'\'', 'Jack-O\\\' (Coming Soon)')
-with open('src/games.ts', 'w', encoding='utf-8') as f:
-    f.write(text)
-
