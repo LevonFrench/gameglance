8e204fd3799cfbad135e761ad365790bcd97fbbd
LevonFrench <foxhoundsounds@gmail.com>
2026-04-27 02:57:38 -0500

    feat: Add GSAP PromoTrailerView for cinematic captures

--- src/marketing-main.tsx
+++ src/marketing-main.tsx
@@ -4,6 +4,8 @@ import * as Sentry from "@sentry/react";
 import './index.css'
 import { LandingView } from './LandingView'
 
+import { PromoTrailerView } from './PromoTrailerView'
+
 Sentry.init({
   dsn: "https://9063c32b9eda7280ab27fcdb6a777f8c@o4511275909054464.ingest.us.sentry.io/4511275922292736",
   integrations: [
@@ -15,10 +17,17 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
   sendDefaultPii: true
 });
+
+const isPromo = window.location.search.includes('promo=true');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LandingView onLaunch={() => {
-      window.location.href = '/';
-    }} />
+    {isPromo ? (
+      <PromoTrailerView />
+    ) : (
+      <LandingView onLaunch={() => {
+        window.location.href = '/';
+      }} />
+    )}
   </StrictMode>,
 )
